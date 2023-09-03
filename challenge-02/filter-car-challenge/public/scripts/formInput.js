03e37758ff1688a08ee3b4217c8e12e008f42764
novalmahardhika <asubasa9@gmail.com>
2023-09-03 13:39:20 +0700

    add script form input for submit data

---
+++ challenge-02/filter-car-challenge/public/scripts/formInput.js
@@ -0,0 +1,40 @@
+const form = document.querySelector('.container-input')
+const modal = document.querySelector('.modal-custom')
+
+// input Form
+const typeDriver = document.querySelector('#inputType')
+const dateInput = document.querySelector('#inputDate')
+const timeInput = document.querySelector('#inputTime')
+const capacityInput = document.querySelector('#inputCapacity')
+
+// Modal
+form.addEventListener('click', () => {
+  modal.style.display = 'block'
+})
+
+window.addEventListener('click', (e) => {
+  if (e.target == modal) {
+    modal.style.display = 'none'
+  }
+})
+
+// form submit
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  modal.style.display = 'none'
+
+  // if (
+  //   typeDriver.value === '' ||
+  //   dateInput.value === '' ||
+  //   timeInput.value === ''
+  // ) {
+  //   console.log('fuck')
+  // }
+
+  console.log(
+    typeDriver.value,
+    dateInput.value,
+    timeInput.value,
+    capacityInput.value
+  )
+})
