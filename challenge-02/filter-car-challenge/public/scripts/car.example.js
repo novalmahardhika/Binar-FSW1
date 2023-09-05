8a754a1229590f7cf4b0aef745e185eb0e54bb8b
novalmahardhika <asubasa9@gmail.com>
2023-09-05 07:32:59 +0700

    add card car in filter page

--- challenge-02/filter-car-challenge/public/scripts/car.example.js
+++ challenge-02/filter-car-challenge/public/scripts/car.example.js
@@ -1,8 +1,8 @@
 class Car {
-  static list = [];
+  static list = []
 
   static init(cars) {
-    this.list = cars.map((i) => new this(i));
+    this.list = cars.map((i) => new this(i))
   }
 
   constructor({
@@ -22,31 +22,60 @@ class Car {
     specs,
     availableAt,
   }) {
-    this.id = id;
-    this.plate = plate;
-    this.manufacture = manufacture;
-    this.model = model;
-    this.image = image;
-    this.rentPerDay = rentPerDay;
-    this.capacity = capacity;
-    this.description = description;
-    this.transmission = transmission;
-    this.available = available;
-    this.type = type;
-    this.year = year;
-    this.options = options;
-    this.specs = specs;
-    this.availableAt = availableAt;
+    this.id = id
+    this.plate = plate
+    this.manufacture = manufacture
+    this.model = model
+    this.image = image
+    this.rentPerDay = new Intl.NumberFormat('id-ID', {
+      style: 'currency',
+      currency: 'IDR',
+    }).format(rentPerDay)
+    this.capacity = capacity
+    this.description = description
+    this.transmission = transmission
+    this.available = available
+    this.type = type
+    this.year = year
+    this.options = options
+    this.specs = specs
+    this.availableAt = availableAt
   }
 
   render() {
     return `
-      <p>id: <b>${this.id}</b></p>
-      <p>plate: <b>${this.plate}</b></p>
-      <p>manufacture: <b>${this.manufacture}</b></p>
-      <p>model: <b>${this.model}</b></p>
-      <p>available at: <b>${this.availableAt}</b></p>
-      <img src="${this.image}" alt="${this.manufacture}" width="64px">
-    `;
+    <div class="card-cars">      
+      <div>
+        <img src="${this.image}" alt="${this.manufacture}" class="img-fluid" >
+        <p>${this.manufacture}/${this.model}</p>
+        <h6>${this.rentPerDay}</h6>
+        <p>${this.description}</p>
+      
+      <div>
+        <img src="./assets/fi_users.svg" style="width:20px; height:20px">
+        <span>${this.capacity} Orang</span>
+      </div>
+
+      <div>
+        <img src="./assets/fi_settings.svg" style="width:20px; height:20px">
+        <span>${this.transmission}</span>
+      </div>
+
+      <div>
+        <img src="./assets/fi_calendar.svg" style="width:20px; height:20px">
+        <span>${this.year}</span>
+      </div>
+
+      </div>
+
+      <button class="btn btn-success">Pilih Mobil</button>
+    </div>
+    `
   }
 }
+
+// <p>id: <b>${this.id}</b></p>
+//       <p>plate: <b>${this.plate}</b></p>
+//       <p>manufacture: <b>${this.manufacture}</b></p>
+//       <p>model: <b>${this.model}</b></p>
+//       <p>available at: <b>${this.availableAt}</b></p>
