b95b026f89ed30e88a504bad1bcb2cb953cbc4a2
novalmahardhika <asubasa9@gmail.com>
2023-09-13 22:17:23 +0700

    add endpoint created cars, update cars, delete cars

--- challenge-03/index.js
+++ challenge-03/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cars = require('./cars.json')
 const app = express()
 const port = 3000
 
@@ -6,6 +7,32 @@ app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
+// get all cars
+app.get('/cars', (req, res) => {
+  res.json(cars)
+})
+
+// get cars by id
+app.get('/cars/:id', (req, res) => {
+  res.json(cars.find((car) => car.id === req.params.id))
+})
+
+// created car
+app.post('/cars', () => {
+  res.json(cars)
+})
+
+// update car
+app.put('/cars/:id', (req, res) => {
+  res.json(cars.find((car) => car.id === req.params.id))
+})
+
+// delete car
+app.delete('/cars/:id', (req, res) => {
+  res.json(cars.find((car) => car.id === req.params.id))
+})
+
+// running server
 app.listen(port, () => {
-  console.log(`Server Running on Port ${3000}`)
+  console.log(`Server Running on http://localhost:${3000}`)
 })
