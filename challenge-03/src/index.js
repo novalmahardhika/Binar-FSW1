d012150fc2df41f5ff189dee808f48d6dd42cdb9
novalmahardhika <asubasa9@gmail.com>
2023-09-14 15:41:22 +0700

    add chalk

--- challenge-03/src/index.js
+++ challenge-03/src/index.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const car = require('../src/routes/carRouter')
+const chalk = require('chalk')
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
 // endpoint
+
+// cars endpoint
 app.use('/cars', car)
 
 // running server
 app.listen(port, () => {
-  console.log(`Server Running on http://localhost:${3000}`)
+  console.log(
+    `Server Running on`,
+    chalk.yellowBright.underline(`http://localhost:${port}`)
+  )
 })
