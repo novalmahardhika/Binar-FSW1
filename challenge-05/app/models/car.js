37594895c753a5907aa1e525822ad9cc26f84b04
novalmahardhika <asubasa9@gmail.com>
2023-10-22 20:56:42 +0700

    add validation in model car

--- challenge-05/app/models/car.js
+++ challenge-05/app/models/car.js
@@ -27,16 +27,78 @@ module.exports = (sequelize, DataTypes) => {
   }
   Car.init(
     {
-      name: DataTypes.STRING,
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'name cannot null',
+          },
+          notEmpty: true,
+        },
+      },
+
       type: {
         type: DataTypes.ENUM('small', 'medium', 'large'),
         defaultValue: 'small',
+        validate: {
+          isIn: {
+            args: [['small', 'medium', 'large']],
+            msg: 'type not valid, type should be small, medium, or large',
+          },
+        },
       },
-      image: DataTypes.STRING,
-      capacity: DataTypes.INTEGER,
-      rentPerDay: DataTypes.INTEGER,
-      description: DataTypes.STRING,
-      availableAt: DataTypes.STRING,
+
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'image cannot null',
+          },
+        },
+      },
+
+      capacity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'capacity cannot null',
+          },
+        },
+      },
+
+      rentPerDay: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'rentPerDay cannot null',
+          },
+        },
+      },
+
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'description cannot null',
+          },
+        },
+      },
+
+      availableAt: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'availableAt cannot null',
+          },
+        },
+      },
+
       createdBy: DataTypes.STRING,
       updatedBy: DataTypes.STRING,
       deletedBy: DataTypes.STRING,
