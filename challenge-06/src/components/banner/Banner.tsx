1e47d0a954fd76827add161b769785f72ffa5831
novalmahardhika <asubasa9@gmail.com>
2023-11-01 18:41:21 +0700

    added font respnsive in banner section

--- challenge-06/src/components/banner/Banner.tsx
+++ challenge-06/src/components/banner/Banner.tsx
@@ -4,7 +4,7 @@ import { Button } from '../ui/button'
 export default function Banner() {
   return (
     <section className='bg-blue-800 mt-[60px]  md:mx-[136px] mx-4 text-center md:mt-[100px] text-white md:px-0 px-[58px] py-16 flex items-center flex-col rounded-[13px]'>
-      <h1 className='font-bold text-2xl md:text-4xl mb-4'>
+      <h1 className='font-bold text-2xlg md:text-4xl mb-4'>
         Sewa Mobil di Tangerang Sekarang
       </h1>
       <p className='text-sm mb-10 max-w-[480px]'>
